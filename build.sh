a74153805defe9864d195201de1f4288a68782d4
Erlael <kuran001@mail.ru>
2020-05-30 02:37:49 +0700

    Edit

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ sudo apt install mono-runtime
 sudo apt-get install nunit-console
 nuget restore Enigma.sln
 msbuild /p:Configuraton=Release Enigma.sln
-cd Erlael/bin/Release
-mono Erlael.exe
+cd Enigma/bin/Release
+mono Enigma.exe
