d07e065462a1c1919301aab6e3143e458130339c
Erlael <kuran001@mail.ru>
2020-06-01 19:54:17 +0700

    start the project from the beginning

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt install nuget
-sudo apt install mono-runtime
-sudo apt-get install nunit-console
-nuget restore Enigma.sln
-msbuild /p:Configuraton=Release Enigma.sln
-cd Enigma/bin/Release
-mono Enigma.exe
