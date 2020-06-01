0386a065b9453c240f2d03b41369f2a063ed37ff
SmirnovaAnastasia <sm.anastasiya2010@yandex.ru>
2020-06-01 21:29:54 +0700

    Work with rotors

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,7 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(446, 197);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(446, 237);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(446, 283);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(446, 332);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 3;
             // 
             // Form1
             // 
@@ -36,13 +68,23 @@
             this.BackColor = System.Drawing.Color.Silver;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(589, 701);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -17,4 +17,32 @@ namespace Enigma
             InitializeComponent();
         }
     }
+
+    private void Rot(string btnText, int b)
+    {
+        int r1 = Convert.ToInt32(textBox1.Text);
+        int r2 = Convert.ToInt32(textBox2.Text);
+        int r3 = Convert.ToInt32(textBox3.Text);
+        int r11 = Program.GetRot(1, r1);
+        int r22 = Program.GetRot(2, r2);
+        int r33 = Program.GetRot(3, r3);
+        int shift = r11 + r22 + r33;
+        string Al;
+        if (b == 0)
+        {
+            Al = Program.Coding(Array.IndexOf(alphabet, btnText), shift, r1, r2, r3);
+        }
+        else
+        {
+            Al = Program.Decoding(Array.IndexOf(alphabet, btnText), shift, r1, r2, r3);
+        }
+        textBox4.Text += Al + " ";
+        r3 = Program.Roters3(r3);
+        r2 = Program.Roters2(r2, r3);
+        r1 = Program.Roters1(r1, r2, r3);
+        textBox1.Text = r1.ToString();
+        textBox2.Text = r2.ToString();
+        textBox3.Text = r3.ToString();
     }
+}
+
