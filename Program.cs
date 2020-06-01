d523361fed7cb500252a54d3507e7144d9d83000
SmirnovaAnastasia <sm.anastasiya2010@yandex.ru>
2020-06-01 20:51:28 +0700

    Add rotor shift function

--- Program.cs
+++ Program.cs
@@ -20,6 +20,25 @@ namespace Enigma
             Application.Run(new Form1());
         }
 
-        
+        public static int GetRot(int r, int number)
+        {
+            int[] rot1 = new int[10] { 8, 3, 2, 6, 9, 0, 1, 4, 5, 7 };
+            int[] rot2 = new int[10] { 4, 8, 1, 3, 9, 0, 2, 5, 6, 7 };
+            int[] rot3 = new int[10] { 14, 1, 6, 2, 8, 2, 4, 5, 2, 10 };
+
+            switch (r)
+            {
+                case 1:
+                    return rot1[number];
+                case 2:
+                    return rot2[number];
+                case 3:
+                    return rot3[number];
+                default:
+                    return 0;
+            }
+        }
+
+
     }
 }
